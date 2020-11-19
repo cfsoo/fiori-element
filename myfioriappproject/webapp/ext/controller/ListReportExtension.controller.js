18b2c75d86f73bcc31aa068e7f23f943156a6ca5
Chuak Fen <chuakfen.soo@rizing.com>
2020-11-19 15:18:09 +0800

    Step 28.	Adding a custom column

--- myfioriappproject/webapp/ext/controller/ListReportExtension.controller.js
+++ myfioriappproject/webapp/ext/controller/ListReportExtension.controller.js
@@ -7,8 +7,9 @@ sap.ui.define([
     "sap/m/Text",
     "sap/m/Button",
     "sap/m/ButtonType",
-    "sap/m/DialogType"
-], function (SmartFilterBar, ComboBox, Filter, SmartField, Dialog, Text, Button, ButtonType, DialogType) {
+    "sap/m/DialogType",
+    "sap/m/MessageBox"
+], function (SmartFilterBar, ComboBox, Filter, SmartField, Dialog, Text, Button, ButtonType, DialogType, MessageBox) {
     "use strict";
 
     return sap.ui.controller("com.rizing.myfioriappproject.ext.controller.ListReportExtension", {
@@ -63,6 +64,14 @@ sap.ui.define([
                 var oContext = oItem.getBindingContext();
                 this._showChangeTotalSalesPopup(oContext);
             }
+        },
+
+        /**
+        * Event handler when user pressed on the Sales Overview icon.
+        * @public
+        */
+        onPressSalesOverview: function () {
+            MessageBox.success(this.getView().getModel("@i18n").getProperty("salesOverviewText"), {});
         },
 
         /**
